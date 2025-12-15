39a28ff2a8cf7f3f5d8a2481e41b8a2e7e666f09
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-12-15 09:15:32 +0100

    add header and banner

---
+++ play-and-win/app/components/TheBanner.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-blue-950 text-white w-full flex flex-row gap-32 justify-center border-b border-gray-400 p-2 text-sm
+    ">
+        <div>
+            <p>
+                <strong>Jeux Olympiques d'hiver™</strong> · Du 6 au 22 février 2026 | <strong>Jeux Paralympiques d'hiver™</strong>· Du 6 au 15 mars 2026
+
+            </p>
+        </div>
+        <div class="flex flex-row gap-8">
+            <a href="#">Tickets</a>
+            <a href="#">Hospitality</a>
+            <a href="#">Boutique</a>
+        </div>
+    </div>
+
+</template>
+
+<style scoped></style>
--- play-and-win/app/components/TheHeader.vue
+++ play-and-win/app/components/TheHeader.vue
@@ -1,21 +1,31 @@
 <script setup>
+import { Menu } from 'lucide-vue-next';
 </script>
 
 <template>
-    <div class="flex flex-row items justify-start gap-8">
-        <div class="flex flex-row  gap-4">
-            <div>
-                <img src="~/assets/logo.png" alt="logo milan cortina">
+    <div class="bg-blue-950 text-white w-full flex flex-row justify-center">
+        <div class="flex flex-row items justify-start gap-48 items-center p-4 mx-8">
+            <div class="flex flex-row  gap-4 items-center">
+                <div class="w-18">
+                    <img src="~/assets/logo.png" alt="logo milan cortina">
+                </div>
+                <div class="flex flex-row gap-6">
+                    <a href="#">Olympic torch Relay</a>
+                    <a href="#">Competition Schedule</a>
+                    <a href="#" class="underline">Play & Win</a>
+                </div>
             </div>
-            <div>
-                <a href="#">Olympic torch Relay</a>
-                <a href="#">Competition Schedule</a>
-                <a href="#" class="font-bold">Play & Win</a>
+            <div class="flex flex-row gap-6 items-center">
+                <div class="border rounded-4xl p-3 bg-white text-blue-950 cursor-pointer hover:bg-gray-200"> <a
+                        href="#">Sign In</a></div>
+                <div class="border rounded-4xl p-3 cursor-pointer hover:bg-blue-900">
+                    <a href="#">English</a>
+                </div>
+                <div class="cursor-pointer hover:text-gray-200">
+                    <Menu />
+                </div>
             </div>
-        </div>
-        <div class="flex flex-row gap-4">
-            <a href="#">Sign In</a>
-            <a href="#">English</a>
+
         </div>
     </div>
 </template>
