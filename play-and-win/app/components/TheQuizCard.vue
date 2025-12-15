aca93f1dfab4406911697c86134f65e874cfceca
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-12-15 11:10:35 +0100

    add quizzes section

---
+++ play-and-win/app/components/TheQuizCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    sportLogo: {
+        type: String,
+        required: true
+    },
+    sportName: {
+        type: String,
+        required: true
+    }
+});
+
+const logoSrc = computed(() => new URL(`../assets/logo-sports/${props.sportLogo}`, import.meta.url).href);
+</script>
+
+<template>
+    <div class="flex flex-col items-center gap-2 rounded-2xl border bg-blue-950 p-4 text-center shadow-sm backdrop-blur-sm hover:scale-105 hover:shadow-md transition-transform">
+        <div class="flex h-25 w-30 items-center justify-center rounded-full text-white">
+            <img :src="logoSrc" :alt="`Logo ${sportName}`" class="object-contain" loading="lazy">
+        </div>
+        <h2 class="text-base font-semibold text-white-950">{{ sportName }}</h2>
+    </div>
+
+</template>
---
+++ play-and-win/app/components/TheQuizzes.vue
@@ -0,0 +1,41 @@
+<script setup>
+import TheQuizCard from './TheQuizCard.vue';
+
+const quizzes = [
+    { sportName: 'Ski alpin', sportLogo: 'ALP.svg' },
+    { sportName: 'Bobsleigh', sportLogo: 'BOB.svg' },
+    { sportName: 'Biathlon', sportLogo: 'BTH.svg' },
+    { sportName: 'Ski de fond', sportLogo: 'CCS.svg' },
+    { sportName: 'Curling', sportLogo: 'CUR.svg' },
+    { sportName: 'Freestyle', sportLogo: 'FRS.svg' },
+    { sportName: 'Patinage artistique', sportLogo: 'FSK.svg' },
+    { sportName: 'Hockey sur glace', sportLogo: 'IHO.svg' },
+    { sportName: 'Luge', sportLogo: 'LUG.svg' },
+    { sportName: 'Combiné nordique', sportLogo: 'NCB.svg' },
+    { sportName: 'Snowboard', sportLogo: 'SBD.svg' },
+    { sportName: 'Saut à ski', sportLogo: 'SJP.svg' },
+    { sportName: 'Skeleton', sportLogo: 'SKN.svg' },
+    { sportName: 'Short track', sportLogo: 'STK.svg' },
+    { sportName: 'Patinage de vitesse', sportLogo: 'SSK.svg' },
+    { sportName: 'Ski de montagne', sportLogo: 'SMT.svg' }
+];
+</script>
+
+<template>
+    <section class="mx-auto w-full max-w-6xl px-4 py-8 flex flex-col items-center">
+        <div class="mb-6 flex items-center justify-between">
+            <h1 class="text-3xl font-bold text-blue-950 m-4">Quiz sur le sport</h1>
+        </div>
+        <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+            <TheQuizCard
+                v-for="quiz in quizzes"
+                :key="quiz.sportLogo"
+                :sport-name="quiz.sportName"
+                :sport-logo="quiz.sportLogo"
+            />
+        </div>
+
+    </section>
+</template>
+
+<style scoped></style>
