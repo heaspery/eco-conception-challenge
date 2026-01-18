b1a603aff323a7efc4fd79d5c206d729cfdb15cd
Mathilde <62794471+heaspery@users.noreply.github.com>
2026-01-18 11:04:45 +0100

    fix images

--- play-and-win/app/components/TheQuizCard.vue
+++ play-and-win/app/components/TheQuizCard.vue
@@ -21,7 +21,7 @@ const props = defineProps({
 <template>
     <div class="cursor-pointer flex flex-col items-center gap-2 rounded-2xl border bg-blue-950 p-4 text-center shadow-sm backdrop-blur-sm hover:scale-105 hover:shadow-md transition-transform">
         <div class="flex h-20 w-20 items-center justify-center rounded-full text-white">
-            <NuxtImg v-if="src" :src="src" :alt="`Logo ${sportName}`" class="h-20 w-20 object-contain" loading="lazy" />
+            <img v-if="src" :src="src" :alt="`Logo ${sportName}`" class="h-20 w-20 object-contain" loading="lazy" />
         </div>
         <h2 class="text-base font-semibold text-white">{{ sportName }}</h2>
     </div>
